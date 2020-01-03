7193811fc517337565d2ddaae4e206e7916b4615
mbura <m.bura@interia.pl>
2020-01-03 11:02:30 +0100

    Updates

---
+++ precache-manifest.fe42be4e97d85cbf11c572fee4d9b11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083ddc6a4494fe638d82d380229a8592",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "502771e642e96bcaf772",
+    "url": "/Monsters-Rolodex/static/css/main.3360b99b.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/Monsters-Rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "502771e642e96bcaf772",
+    "url": "/Monsters-Rolodex/static/js/main.fadd8155.chunk.js"
+  },
+  {
+    "revision": "158fd0877078ccfb2131",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f6db19d2.js"
+  }
+]);
